9aaa5d196b248746d13d18b975c40ca19577b862
abhisit snk <abhisit2537@hotmail.com>
2018-02-02 15:24:29 +0700

    Order cancel Alert controller

--- src/pages/order-detail/order-detail.ts
+++ src/pages/order-detail/order-detail.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { OrderDetailModel } from '../../assets/model/order-detail.model';
 import { OrderServiceProvider } from '../order/order-service';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the OrderDetailPage page.
@@ -16,11 +17,14 @@ import { OrderServiceProvider } from '../order/order-service';
   templateUrl: 'order-detail.html',
 })
 export class OrderDetailPage {
+  testRadioResult: string = '';
+  testRadioOpen: boolean = false;
   order: OrderDetailModel = new OrderDetailModel();
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public orderService: OrderServiceProvider
+    public orderService: OrderServiceProvider,
+    public alertCtrl: AlertController
   ) {
     // this.navParams.data;
     this.orderService.getOrderDetail().then((data) => {
@@ -32,6 +36,44 @@ export class OrderDetailPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OrderDetailPage');
+  }
+  doRadio() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('เหตุผลในการปฏิเสธสินค้า');
+
+    alert.addInput({
+      type: 'radio',
+      label: 'สินค้าหมด',
+      value: 'value1',
+      checked: true
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'ไม่มีสินค้า',
+      value: 'value2'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'ร้านค้าปิด',
+      value: 'value3'
+    });
+
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'Ok',
+      handler: (data: any) => {
+        console.log('Radio data:', data);
+        this.testRadioOpen = false;
+        this.testRadioResult = data;
+      }
+    });
+
+    alert.present().then(() => {
+      this.testRadioOpen = true;
+    });
   }
 
 }
