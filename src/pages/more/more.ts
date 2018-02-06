66959524a8f5214b81d162b2e7f9245437414a9b
Sirintra Wannakheaw <sirintrawannakeaw@gmail.com>
2018-02-06 13:45:22 +0700

    update more

--- src/pages/more/more.ts
+++ src/pages/more/more.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
+import { UserModel } from '../../assets/model/user.model';
 /**
  * Generated class for the MorePage page.
  *
@@ -13,13 +14,15 @@ import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
   templateUrl: 'more.html',
 })
 export class MorePage {
+  user: UserModel = new UserModel();
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams, 
     public app: App
   ) {
   }
-  ionViewDidLoad() {
+  ionViewWillEnter() {
+    this.user = window.localStorage.getItem('jjbiz-user') ? JSON.parse(window.localStorage.getItem('jjbiz-user')) : null;
     console.log('ionViewDidLoad MorePage');
   }
   logout() {
@@ -33,5 +36,8 @@ export class MorePage {
   }
   onSetting() {
     this.app.getRootNav().push('SettingDetailPage');
+  }
+  editProfile() {
+    this.navCtrl.push('ProfilePage');
   }
 }
